8acb3b47364a09010593e81f9ad8b356853b3a15
Sj <sjmrstevens@gmail.com>
2015-11-04 14:27:27 -0600

    added PHP CLI argument for highlow exercise

--- highlow.php
+++ highlow.php
@@ -1,18 +1,47 @@
 <?php
 
-$random = mt_rand(1, 100);
+var_dump($argv);
 
-do	{fwrite(STDOUT, 'Guess a number ');
+if ($argc < 3 || (!is_numeric($argv[1]) || !is_numeric($argv[2]))) {
+	exit("Please give me two numbers!" . PHP_EOL);
+}
 
-	$guess = fgets(STDIN);
+if ($argv[1] > $argv[2]){
+	$max = $argv[1];
+	$min = $argv[2];
+} else {
+	$max = $argv[2];
+	$min = $argv[1];
+}
 
-	if ($guess == $random){
-		fwrite(STDOUT, "Correct!");
+$random = mt_rand($min, $max);
+
+$guesscount = 1;
+
+do	{
+	fwrite(STDOUT, 'Guess a number ');
+
+	$guess = trim(fgets(STDIN));
+
+	if (!is_numeric($guess)){
+		fwrite(STDOUT, "Try a number between {$min} and {$max}" . PHP_EOL);
+	} else if ($guess == $random){
+			if ($guesscount == 1){
+			fwrite(STDOUT, "YOU'RE A WIZARD HARRY! FIRST TRY" . PHP_EOL);
+			} else if($guesscount < 4){
+			fwrite(STDOUT, "GOOD GUESS, Correct! It only took you {$guesscount} tries".PHP_EOL);
+			} else if($guesscount > 10){
+			fwrite(STDOUT, "Finally right, I can't believe it took you {$guesscount} tries.".PHP_EOL);
+			} else{
+			fwrite(STDOUT, "Correct, it took you {$guesscount} tries." . PHP_EOL);
+			}
 		} else if($guess > $random){
 			fwrite(STDOUT, "Too High! ");
 		} else if($guess < $random){
 			fwrite(STDOUT, "Too Low! ");
-		}
+		} 
+	$guesscount+=1;
+
 } while ($guess != $random);
 
 ?>
